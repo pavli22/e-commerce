123dca94da730e6c7ad81731dc08dab75d810b67
pavli22 <balysamy651@gmail.com>
2023-10-11 21:32:03 +0300

    second

--- src/app/components/scroll-to-top/scroll-to-top.component.ts
+++ src/app/components/scroll-to-top/scroll-to-top.component.ts
@@ -23,7 +23,7 @@ export class ScrollToTopComponent {
       var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
       if (currentScroll > 0) {
         window.requestAnimationFrame(smoothscroll);
-        window.scrollTo(0, currentScroll - (currentScroll));
+        window.scrollTo(0, currentScroll - (currentScroll / 8));
       }
     })();
   }
